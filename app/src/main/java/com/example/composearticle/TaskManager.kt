e591cb249297de451c42c824f920f1967974f29d
Mohammed Fareed <mohammedfareed3214@gmail.com>
2023-03-19 21:01:51 +0200

    setting Task Manager Activity commit

--- app/src/main/java/com/example/composearticle/TaskManager.kt
+++ app/src/main/java/com/example/composearticle/TaskManager.kt
@@ -3,13 +3,23 @@ package com.example.composearticle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composearticle.ui.theme.ComposeArticleTheme
 
 class TaskManager : ComponentActivity() {
@@ -19,7 +29,7 @@ class TaskManager : ComponentActivity() {
             ComposeArticleTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    TaskCompleted()
                 }
             }
         }
@@ -27,14 +37,27 @@ class TaskManager : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TaskCompleted() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        val image = painterResource(id = R.drawable.ic_task_completed)
+        Image(painter = image, contentDescription = null)
+        Text(
+            text = stringResource(R.string.firstText),
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(24.dp, 8.dp)
+        )
+        Text(text = stringResource(R.string.secondText), fontSize = 16.sp)
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     ComposeArticleTheme {
-        Greeting("Android")
+        TaskCompleted()
     }
 }
